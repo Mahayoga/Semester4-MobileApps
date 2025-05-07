3b13e2c01e876c1b3c34efba5c1fce2bc982b262
Mahayoga <myoga.bahtiar@gmail.com>
2025-05-07 16:47:37 +0700

    Update: Ngerapikan dikit (Aslinya salah branch)

--- lib/page/login/login.dart
+++ lib/page/login/login.dart
@@ -17,106 +17,120 @@ class _LoginPage extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-      body: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Container(
-          width: 360,
-          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 40),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                "Selamat Datang Kembali",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey.shade800,
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center, 
+        children: [
+          Container(
+            width: 360,
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Selamat Datang Kembali",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade800,
+                  ),
                 ),
-              ),
-              SizedBox(height: 8),
-              RichText(
-                text: TextSpan(
-                  text: "Belum mempunyai akun? ",
-                  style: TextStyle(color: Colors.black87),
-                  children: [
-                    TextSpan(
-                      text: "Daftar disini",
-                      style: TextStyle(color: Colors.purple),
+                SizedBox(height: 8),
+                RichText(
+                  text: TextSpan(
+                    text: "Belum mempunyai akun? ",
+                    style: TextStyle(color: Colors.black87),
+                    children: [
+                      TextSpan(
+                        text: "Daftar disini",
+                        style: TextStyle(color: Colors.purple),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.pushNamed(context, '/daftar');
+                          },
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 24),
+                // Email field
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade200,
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Colors.grey.shade400),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: TextField(
+                    controller: emailC,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Email",
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                // Password field
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade200,
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Colors.grey.shade400),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: TextField(
+                    controller: passwordC,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Password",
+                    ),
+                  ),
+                ),
+                SizedBox(height: 12),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Lupa Password?',
                       recognizer: TapGestureRecognizer()
                         ..onTap = () {
-                          Navigator.pushNamed(context, '/daftar');
+                          Navigator.pushNamed(context, '/forgot-password');
                         },
+                      style: TextStyle(
+                        color: Colors.purple
+                      )
                     ),
-                  ],
+                  )
                 ),
-              ),
-              SizedBox(height: 24),
-              // Email field
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all(color: Colors.grey.shade400),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: TextField(
-                  controller: emailC,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Email",
+                SizedBox(height: 24),
+                ElevatedButton(
+                  onPressed: () {
+                    handleLogin(context, emailC, passwordC);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    minimumSize: Size(double.infinity, 48),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    elevation: 8,
+                    shadowColor: Colors.purpleAccent,
                   ),
+                  child: Text("Lanjutkan", style: TextStyle(color: Colors.white)),
                 ),
-              ),
-              SizedBox(height: 16),
-              // Password field
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all(color: Colors.grey.shade400),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: TextField(
-                  controller: passwordC,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Password",
-                  ),
-                ),
-              ),
-              SizedBox(height: 12),
-              Align(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  "Lupa Password?",
-                  style: TextStyle(color: Colors.purple),
-                ),
-              ),
-              SizedBox(height: 24),
-              ElevatedButton(
-                onPressed: () {
-                  handleLogin(context, emailC, passwordC);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.purple,
-                  minimumSize: Size(double.infinity, 48),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  elevation: 8,
-                  shadowColor: Colors.purpleAccent,
-                ),
-                child: Text("Lanjutkan", style: TextStyle(color: Colors.white)),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ]),
+        ]
+      ),
     );
   }
 }
@@ -139,11 +153,25 @@ Future<void> handleLogin(BuildContext context, TextEditingController emailC, Tex
   if(responses.statusCode == 200) {
     Map<String, dynamic> theData = jsonDecode(responses.body);
     if(theData['status'] == 'success') {
-      Navigator.pushNamed(context, '/dashboard');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.green,
+          showCloseIcon: true,
+          content: Text('Login berhasil')
+        ),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Session Gagal')
+        ),
+      );
+      // Navigator.pushNamed(context, '/dashboard');
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('Kesalahan terjadi (bukan catch)')
+          backgroundColor: Colors.red,
+          showCloseIcon: true,
+          content: Text('Kesalahan terjadi: ${theData["msg"]}')
         ),
       );
     }
@@ -152,7 +180,9 @@ Future<void> handleLogin(BuildContext context, TextEditingController emailC, Tex
     if (!context.mounted) return;
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text('Error: ${e.toString()}')
+        backgroundColor: Colors.red,
+        showCloseIcon: true,
+        content: Text('Error: Koneksi gagal, silahkan coba lagi nanti')
       ),
     );
   }
--- lib/page/login/lupa_password.dart
+++ lib/page/login/lupa_password.dart
@@ -8,55 +8,60 @@ class ForgotPasswordScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(leading: BackButton()),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.lock_outline, size: 100, color: Colors.purple),
-            SizedBox(height: 20),
-            Text(
-              'Lupa Password?',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text('Tolong masukkan email anda untuk menerima kode verifikasi.'),
-            SizedBox(height: 20),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextButton(
-              onPressed: () {},
-              child: Text('Coba cara lain', style: TextStyle(color: Colors.purple)),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => VerificationScreen()), // <-- sekarang aman!
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.purple,
-                minimumSize: Size(double.infinity, 50),
-              ),
-              child: const Text(
-                  'Kirim',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 360,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.lock_outline, size: 100, color: Colors.purple),
+                  SizedBox(height: 20),
+                  Text(
+                    'Lupa Password?',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                   ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Tolong masukkan email anda untuk menerima kode verifikasi.',
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => VerificationScreen()), // <-- sekarang aman!
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.purple,
+                      minimumSize: Size(double.infinity, 50),
+                    ),
+                    child: const Text(
+                        'Kirim',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold
+                        ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
+          )
+        ],
+      )
     );
   }
 }
