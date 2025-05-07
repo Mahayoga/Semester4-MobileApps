1aff78284df8a290ef944b15525da623c00986ef
Mahayoga <myoga.bahtiar@gmail.com>
2025-05-07 18:00:25 +0700

    Add: Animasi loading saat login

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Contoh Routing Flutter',
       initialRoute: '/',
       routes: {
